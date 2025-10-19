5e85fa94e8f87f4374aa94de952a782b97a02cb2
Ana Luísa :0 <analuisacostanunes@gmail.com>
2025-10-19 20:15:55 -0300

    .

--- employee_attrition_project/pipeline/main.py
+++ employee_attrition_project/pipeline/main.py
@@ -7,40 +7,44 @@ from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 from sqlalchemy import create_engine
 
-filename = "credentials.json"
-scopes = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
+def load_data_from_drive(folder_id: str):
+    filename = "credentials.json"
+    scopes = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
 
-env_path = os.path.join(os.path.dirname(__file__), ".env")
-load_dotenv(dotenv_path=env_path)
-creds_path = os.getenv("GOOGLE_CREDS_PATH")
+    env_path = os.path.join(os.path.dirname(__file__), ".env")
+    load_dotenv(dotenv_path=env_path)
+    creds_path = os.getenv("GOOGLE_CREDS_PATH")
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scopes)
-client = gspread.authorize(creds)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scopes)
+    client = gspread.authorize(creds)
 
-# Drive
-drive = build('drive', 'v3', credentials=creds)
-folder_id = "1OPXB9-peoHdgrXjSMGidl3azmiHA6YXB"
+    drive = build('drive', 'v3', credentials=creds)
+    folder_id = "1OPXB9-peoHdgrXjSMGidl3azmiHA6YXB"
 
-results = drive.files().list(
-    q=f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.spreadsheet'",
-    fields="files(id, name)"
-    ).execute()
+    results = drive.files().list(
+        q=f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.spreadsheet'",
+        fields="files(id, name)"
+        ).execute()
 
-files = results.get('files', [])
+    files = results.get('files', [])
 
-# DataFrames
-dataframes = {}
-for file in files:
-    sheet = client.open_by_key(file['id']).sheet1
-    df = pd.DataFrame(sheet.get_all_records())
-    dataframes[file['name']] = df
+    dataframes = {}
+    for file in files:
+        sheet = client.open_by_key(file['id']).sheet1
+        df = pd.DataFrame(sheet.get_all_records())
+        dataframes[file['name']] = df
 
-connection_str = f"postgresql://user:4QLOEHPgwgJsXkrE6qnyh44VUCv2o7AB@dpg-d3nc4pje5dus738tm8qg-a.oregon-postgres.render.com/employee_attrition"
+    return dataframes
 
-engine = create_engine(connection_str)
-for name, df in dataframes.items(): 
-    table_name = name.lower().replace(" ", "_")
-    df.to_sql(table_name, engine, if_exists='replace', index=False)
+def save_data_to_postgres(dataframes: dict, db_url: str):
+    db_url = os.getenv("DB_URL")
+    connection_str = f"db_url"
+
+    engine = create_engine(connection_str)
+    for name, df in dataframes.items(): 
+        table_name = name.lower().replace(" ", "_")
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
+    
